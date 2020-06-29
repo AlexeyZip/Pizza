5ae3a12b9f31edd0190c55a9ca9c577338e7c6cd
AlexeyZip <alexzipzap@gmail.com>
2020-06-29 16:48:29 +0300

    n4

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Button} from './Button'
 export {default as CategoriesList} from './CategoriesList'
 export {default as Header} from './Header'
+export {default as SortPopup} from './SortPopup'
